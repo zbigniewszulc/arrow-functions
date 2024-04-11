9ad1aff249651e6f8fd9a1232da04caa499b2da9
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-04-11 20:58:50 +0000

    rest parameter sytax

---
+++ rest-parameter-syntax.js
@@ -0,0 +1,24 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+const sumAll = ( a, b, c ) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
+
+// Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2", sum2);
+
+// Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+} 
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3);
