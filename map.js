9bf0eb2f58a5637c91964e4aa1a7709e88ddab9c
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-04-15 23:23:09 +0000

    mapping - more examples

--- map.js
+++ map.js
@@ -5,16 +5,28 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function(num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
-
+const simplfied = nums.map(function (num) { return num * 2});
+console.log(simplfied);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -38,4 +50,5 @@ const students = [
     },
   ];
 
-    
+const studentsWithIds =  students.map(student => [student.name, student.id]);    
+console.log(studentsWithIds);
